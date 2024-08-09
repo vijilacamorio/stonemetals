85799c429c74520dc4ee552f7ff7ecadd63683b4
vijila <cvijila@amoriotech.com>
2024-08-09 15:29:47 +0530

    initial commit

---
+++ application/views/layout/footer.php
@@ -0,0 +1,23 @@
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <!-- Vendor JS Files -->
+  <script src="<?php echo base_url();?>assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="<?php echo base_url();?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo base_url();?>assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="<?php echo base_url();?>assets/vendor/echarts/echarts.min.js"></script>
+  <script src="<?php echo base_url();?>assets/vendor/quill/quill.js"></script>
+  <script src="<?php echo base_url();?>assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="<?php echo base_url();?>assets/vendor/php-email-form/validate.js"></script>
+  <script src="<?php echo base_url();?>assets/js/main.js"></script>
+  <script src="<?php echo base_url();?>assets/js/jquery.js"></script>
+  <script src="<?php echo base_url();?>assets/js/sweet_alert.js"></script>
+  <script type="text/javascript" charset="utf8" src=" <?php echo base_url();?>assets/vendor/datatables/jquery.dataTables.js"></script>
+  <script type="text/javascript" charset="utf8" src=" <?php echo base_url();?>assets/vendor/datatables/dataTables.colReorder.min.js"></script>
+  <script type="text/javascript" src=" <?php echo base_url();?>assets/vendor/datatables/dataTables.buttons.min.js"></script>
+  <script type="text/javascript" src=" <?php echo base_url();?>assets/vendor/datatables/buttons.colVis.min.js"></script>
+  <script src=" <?php echo base_url();?>assets/vendor/datatables/buttons.html5.min.js"></script>
+  <script src=" <?php echo base_url();?>assets/vendor/datatables/buttons.print.min.js"></script>
+
+</body>
+
+</html>
---
+++ application/views/layout/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title><?php echo $page_title; ?></title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="<?php echo base_url();?>assets/img/favicon.png" rel="icon">
+  <link href="<?php echo base_url();?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="<?php echo base_url();?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>assets/vendor/datatables/jquery.dataTables.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>assets/vendor/datatables/colReorder.dataTables.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href=" <?php echo base_url();?>assets/vendor/datatables/buttons.dataTables.min.css">
+  <link href="<?php echo base_url();?>backend/assets/css/style.css" rel="stylesheet">
+
+
+</head>
+
+<body>
