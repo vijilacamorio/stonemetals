42c54445d64516c39fd0722d9004d21d1d587f81
ajith <ajith@amoriotech.com>
2024-08-22 18:56:23 +0530

    msg

--- application/views/blog.php
+++ application/views/blog.php
@@ -1,30 +1,22 @@
-<div class="section-full welcome-section-outer">
-        <br>
-        <div style="background: black;height: 75px;padding-top: 20px;">
-          <h3 style="text-align:center;color:white;"><?php echo $page_title; ?></h3>
+<div style="background: black;height: 75px;padding-top: 20px;">
+        <h3 style="text-align:center;color:white;"><?php echo $page_title; ?></h3>
         </div>
-        
-            <div class="welcome-section-top bg-white p-t80 p-b50">
-                <div class="container">
-                    <div class="row d-flex justify-content-center">
-                    <?php
-                        if(!empty($blog)){
-                          foreach($blog as $blogd){
-                            ?>
-                        <div class="col-lg-7 col-md-12 m-b30">
-                        
-                            <div class="welcom-to-section">
-                               <p style="text-align:justify;">Date: <?php echo date('d-m-Y', strtotime($blogd['modified_date'])); ?></p>
-                               <p style="text-align:justify;"><?php echo $blogd['short_description']; ?></p>
-                               <p style="text-align:justify;"><a href="<?php echo base_url('blog/'.$blogd['slug']); ?>">Read More</a></p>
-                             </div>
-                        </div>
-                        <div class="col-lg-5 col-md-12 m-b30">
-                        <img src="<?php echo base_url() . BLOG_IMG_PATH . $blogd['featured_image']; ?>" alt="" style="margin-left: 3px;margin-top: 8px;">
-                        </div> 
-                      <?php } 
-                      } ?>     
-                    </div>
-                </div> 
-            </div>
-        </div> 
+<div class="container mt-5 mb-5" >
+<?php if (!empty($blog)) { ?>
+  <div class="row">
+    <?php foreach ($blog as $blogd) { ?>
+      <div class="col-12 col-md-4" style="border: 1px solid;">
+        <img src="<?php echo base_url() . BLOG_IMG_PATH . $blogd['featured_image']; ?>" alt="" style="margin-left: 3px; margin-top: 8px;">
+        <div class="card-body">          
+          <h3 class="card-title"><?php echo $blogd['title']; ?></h3>
+          <p class="card-text"><?php echo $blogd['short_description']; ?></p>
+          <button type="button" class="btn btn-dark "><a href="<?php echo base_url('blog/'.$blogd['slug']); ?>" class="text-white">Read More</a></button>
+
+          
+        </div>
+      </div>
+    <?php } ?>
+  </div>
+<?php } ?>
+
+</div>
